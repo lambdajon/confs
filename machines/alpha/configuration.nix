83382c43b6ab2c3fb0a4aab16d48c9ec903cf587
Akhmadiy <akhmadiy@Akhmadiys-MacBook-Pro.local>
2024-09-20 16:58:48 +0500

    remove comments

--- machines/alpha/configuration.nix
+++ machines/alpha/configuration.nix
@@ -85,7 +85,6 @@
     powerManagement.enable = false;
     powerManagement.finegrained = false;
     nvidiaSettings = true;
-    # package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
   hardware.bluetooth.settings = {
