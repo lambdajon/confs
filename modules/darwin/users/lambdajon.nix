3d74d6ea5cfa3b04a32702f14e15ba5241b00109
lambdajon <lambdajon42@gmail.com>
2025-09-15 20:49:10 +0500

    add darwin user

---
+++ modules/darwin/users/lambdajon.nix
@@ -0,0 +1,37 @@
+{
+  lib,
+  inputs,
+  outputs,
+  ...
+}: {
+  config = {
+    system.primaryUser = "lambdajon";
+
+    # Available users in the machine
+    users.users = {
+      sakhib = {
+        home = "/Users/lambdajon";
+
+        openssh.authorizedKeys.keys = lib.strings.splitString "\n" (
+          builtins.readFile (
+            builtins.fetchurl {
+              url = "https://github.com/lambdajon.keys";
+              sha256 = "05rvkkk382jh84prwp4hafnr3bnawxpkb3w6pgqda2igia2a4865";
+            }
+          )
+        );
+      };
+    };
+
+    # Home manager configuration for users
+    home-manager = {
+      extraSpecialArgs = {
+        inherit inputs outputs;
+      };
+      users = {
+        # Import your home-manager configuration
+        sakhib = import ../../../home.nix;
+      };
+    };
+  };
+}
