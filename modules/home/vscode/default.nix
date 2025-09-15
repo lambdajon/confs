dba90cad502729d652a05b1f878dbe964303ef0d
lambdajon <lambdajon42@gmail.com>
2025-09-15 18:44:04 +0500

    add vscode

--- modules/home/vscode/default.nix
+++ modules/home/vscode/default.nix
@@ -39,7 +39,33 @@
       pkief.material-icon-theme
       streetsidesoftware.code-spell-checker
       zhuangtongfa.material-theme
-    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [ ];
+      ziglang.vscode-zig
+    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [ 
+      {
+          name = "lean4";
+          publisher = "leanprover";
+          version = "0.0.178";
+          sha256 = "ByhiTGwlQgNkFf0BirO+QSDiXbQfR6RLQA8jM4B1+O4=";
+        }
+        {
+          name = "markdown-checkbox";
+          publisher = "bierner";
+          version = "0.4.0";
+          sha256 = "AoPcdN/67WOzarnF+GIx/nans38Jan8Z5D0StBWIbkk=";
+        }
+        {
+          name = "markdown-preview-github-styles";
+          publisher = "bierner";
+          version = "2.0.3";
+          sha256 = "yuF6TJSv0V2OvkBwqwAQKRcHCAXNL+NW8Q3s+dMFnLY=";
+        }
+        {
+          name = "markdowntable";
+          publisher = "takumii";
+          version = "0.11.0";
+          sha256 = "kn5aLRaxxacQMvtTp20IdTuiuc6xNU3QO2XbXnzSf7o=";
+        }
+    ];
 
     # User defined setings (raw json)
     userSettings = builtins.fromJSON (builtins.readFile ./settings.json);
