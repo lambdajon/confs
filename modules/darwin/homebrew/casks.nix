d40423cfc14a9c263ed30ed6cf0c562df5084682
λjon <46736978+lambdajon@users.noreply.github.com>
2025-09-17 16:26:29 +0500

    fix: remove unnecessary casks

--- modules/darwin/homebrew/casks.nix
+++ modules/darwin/homebrew/casks.nix
@@ -4,36 +4,18 @@
   ...
 }: let
   apps = lib.mkIf config.homebrew.enable [
-    "anki"
-    "anydesk"
-    "balenaetcher"
-    "cleanmymac"
     "discord"
     "element"
-    "elmedia-player"
-    "folx"
     "font-sf-mono-nerd-font-ligaturized"
     "github"
     "iterm2"
     "keka"
     "kekaexternalhelper"
-    "little-snitch"
     "logitech-options"
     "macs-fan-control"
     "obs"
-    "openscad"
-    "openvpn-connect"
-    "parallels"
-    "periphery"
-    "prismlauncher"
-    "raspberry-pi-imager"
-    "rectangle-pro"
     "sf-symbols"
-    "sketch"
-    "xcodes"
-    "wireguard"
     "zen"
-    "zoom"
   ];
 in {
   # Homebrew Casks installations
