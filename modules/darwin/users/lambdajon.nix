c20a4c3e8d39827b42135d68555018f0e893c230
lambdajon <lambdajon42@gmail.com>
2025-09-17 10:30:00 +0500

    change hash my keys

--- modules/darwin/users/lambdajon.nix
+++ modules/darwin/users/lambdajon.nix
@@ -16,7 +16,7 @@
           builtins.readFile (
             builtins.fetchurl {
               url = "https://github.com/lambdajon.keys";
-              sha256 = "05rvkkk382jh84prwp4hafnr3bnawxpkb3w6pgqda2igia2a4865";
+              sha256 = "5d4939839fb6eddf632975d6dbb61b2d58e6c6d832dc8bf49066e10772191e40";
             }
           )
         );
