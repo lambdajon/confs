5f938dd0bb6cd0bc60ddfed004364730d7a6726a
lambdajon <lambdajon42@gmail.com>
2025-09-20 15:06:12 +0500

    chore: trusted public keys & substituters

--- modules/darwin/nixpkgs/default.nix
+++ modules/darwin/nixpkgs/default.nix
@@ -58,11 +58,24 @@
 
       # Additional settings
       settings = {
+        sandbox = true;
+
         # Enable flakes and new 'nix' command
         experimental-features = "nix-command flakes";
+
+        trusted-public-keys = [
+          "hackage-server.cachix.org-1:iw0iRh6+gsFIrxROFaAt5gKNgIHejKjIfyRdbpPYevY="
+          "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+          "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+        ];
         # Trusted users for secret-key
         trusted-users = [
           "${config.users.users.lambdajon.name}"
+        ];
+        substituters = [
+          "https://hackage-server.cachix.org/"
+          "https://cache.iog.io"
+          "https://cache.garnix.io"
         ];
       };
     };
