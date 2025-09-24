3d2fe037abdc9b7fccef7a8c50de8161bd3d6391
lambdajon <lambdajon42@gmail.com>
2025-09-24 21:18:06 +0500

    enable nixos modules

--- flake.nix
+++ flake.nix
@@ -117,7 +117,7 @@
 
       # Reusable nixos modules you might want to export
       # These are usually stuff you would upstream into nixpkgs
-      # nixosModules = import ./modules/nixos;
+      nixosModules = import ./modules/nixos;
 
       # Reusable darwin modules you might want to export
       # These are usually stuff you would upstream into nixpkgs
