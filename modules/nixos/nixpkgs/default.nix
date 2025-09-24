b15f6b581ceb8e6cb4070f9b8e665c4f76ba3fb1
lambdajon <lambdajon42@gmail.com>
2025-09-24 21:11:12 +0500

    configure nixpkgs

---
+++ modules/nixos/nixpkgs/default.nix
@@ -0,0 +1,58 @@
+{
+  lib,
+  config,
+  inputs,
+  ...
+}: {
+  config = {
+    nixpkgs = {
+      # You can add overlays here
+      overlays = [
+        # Add overlays your own flake exports (from overlays and pkgs dir):
+        inputs.orzklv-pkgs.overlays.unstable
+        inputs.orzklv-pkgs.overlays.additions
+        inputs.orzklv-pkgs.overlays.modifications
+
+        # You can also add overlays exported from other flakes:
+        # neovim-nightly-overlay.overlays.default
+
+        # Or define it inline, for example:
+        # (final: prev: {
+        #   hi = final.hello.overrideAttrs (oldAttrs: {
+        #     patches = [ ./change-hello-to-hi.patch ];
+        #   });
+        # })
+      ];
+      # Configure your nixpkgs instance
+      config = {
+        # Disable if you don't want unfree packages
+        allowUnfree = true;
+        # Disable if you don't want linux thingies on mac
+        allowUnsupportedSystem = true;
+        # Workaround for https://github.com/nix-community/home-manager/issues/2942
+        allowUnfreePredicate = _: true;
+        # Let the system use fucked up programs
+        allowBroken = true;
+      };
+    };
+
+    nix = {
+      # This will add each flake input as a registry
+      # To make nix3 commands consistent with your flake
+      # registry = lib.mapAttrs (_: value: {flake = value;}) inputs;
+
+      # This will additionally add your inputs to the system's legacy channels
+      # Making legacy nix commands consistent as well, awesome!
+      nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
+
+      settings = {
+        # Enable flakes and new 'nix' command
+        experimental-features = "nix-command flakes";
+        # Trusted users for secret-key
+        trusted-users = [
+          "${config.users.users.lambdajon.name}"
+        ];
+      };
+    };
+  };
+}
