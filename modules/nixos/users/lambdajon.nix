b697ffabb1ea2fcee1d8233536fd43c74ffb1c77
lambdajon <lambdajon42@gmail.com>
2025-12-25 17:40:41 +0500

    disable ssh

--- modules/nixos/users/lambdajon.nix
+++ modules/nixos/users/lambdajon.nix
@@ -56,14 +56,14 @@ in {
           "libvirtd"
         ];
 
-        openssh.authorizedKeys.keys = lib.strings.splitString "\n" (
-          builtins.readFile (
-            builtins.fetchurl {
-              url = "https://github.com/lambdajon.keys";
-              sha256 = "440dac32fb3ecd060c17f78ad7c34422fefaaccf525c75c3c8dfd5ce86ef516e";
-            }
-          )
-        );
+        # openssh.authorizedKeys.keys = lib.strings.splitString "\n" (
+        #   builtins.readFile (
+        #     builtins.fetchurl {
+        #       url = "https://github.com/lambdajon.keys";
+        #       sha256 = "440dac32fb3ecd060c17f78ad7c34422fefaaccf525c75c3c8dfd5ce86ef516e";
+        #     }
+        #   )
+        # );
       };
     };
 
