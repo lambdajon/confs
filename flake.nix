6768f8822dc2b3900531d068ab52e71d7b222644
lambdajon <lambdajon42@gmail.com>
2025-12-25 15:13:44 +0500

    tower configurations ready

--- flake.nix
+++ flake.nix
@@ -142,7 +142,7 @@
       nixosConfigurations."nixos" = inputs.nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          ./nixos/victus/configuration.nix
+          ./nixos/tower/configuration.nix
         ];
         specialArgs = {
           inherit inputs outputs;
