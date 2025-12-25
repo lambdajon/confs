996ce4e9561a5dd90c838b76c61d2f634335b71b
lambdajon <lambdajon42@gmail.com>
2025-12-25 16:22:15 +0500

    tower configurations ready

--- nixos/tower/hardware-configuration.nix
+++ nixos/tower/hardware-configuration.nix
@@ -4,12 +4,14 @@
 {
   config,
   lib,
-  pkgs,
   modulesPath,
+  inputs,
   ...
 }: {
   imports = [
+    inputs.disko.nixosModules.disko
     (modulesPath + "/installer/scan/not-detected.nix")
+    ./disk-configuration.nix
   ];
 
   boot.initrd.availableKernelModules = ["vmd" "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod"];
@@ -17,20 +19,20 @@
   boot.kernelModules = ["kvm-intel"];
   boot.extraModulePackages = [];
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/1eaeef9c-2dd6-4910-8788-c3d8a684b4ce";
-    fsType = "ext4";
-  };
+  # fileSystems."/" = {
+  #   device = "/dev/disk/by-uuid/1eaeef9c-2dd6-4910-8788-c3d8a684b4ce";
+  #   fsType = "ext4";
+  # };
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/3735-C530";
-    fsType = "vfat";
-    options = ["fmask=0077" "dmask=0077"];
-  };
+  # fileSystems."/boot" = {
+  #   device = "/dev/disk/by-uuid/3735-C530";
+  #   fsType = "vfat";
+  #   options = ["fmask=0077" "dmask=0077"];
+  # };
 
-  swapDevices = [
-    {device = "/dev/disk/by-uuid/cc86a8b4-9910-43e1-b3f9-2fe83f377807";}
-  ];
+  # swapDevices = [
+  #   {device = "/dev/disk/by-uuid/cc86a8b4-9910-43e1-b3f9-2fe83f377807";}
+  # ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
