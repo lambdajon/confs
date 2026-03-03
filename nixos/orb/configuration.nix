dc14d934ad587b03c77ea582a779f72bf15e17fb
lambdajon <lambdajon42@gmail.com>
2026-03-03 13:27:17 +0500

    enable crash

--- nixos/orb/configuration.nix
+++ nixos/orb/configuration.nix
@@ -26,7 +26,7 @@
     inputs.crashes.nixosModules.segfault
 
   ];
-  services.xinux-segfault.enable = false;
+  services.xinux-segfault.enable = true;
   
   users.users.lambdajon = {
     uid = 501;
