c2666147012da4c6c71266dcffcd2ea4aa6bc3b9
lambdajon <lambdajon42@gmail.com>
2026-03-13 21:08:41 +0500

    configure basic configuration

--- nixos/tower2/hardware-configuration.nix
+++ nixos/tower2/hardware-configuration.nix
@@ -19,28 +19,7 @@
   boot.kernelModules = ["kvm-intel"];
   boot.extraModulePackages = [];
 
-  # fileSystems."/" = {
-  #   device = "/dev/disk/by-uuid/1eaeef9c-2dd6-4910-8788-c3d8a684b4ce";
-  #   fsType = "ext4";
-  # };
-
-  # fileSystems."/boot" = {
-  #   device = "/dev/disk/by-uuid/3735-C530";
-  #   fsType = "vfat";
-  #   options = ["fmask=0077" "dmask=0077"];
-  # };
-
-  # swapDevices = [
-  #   {device = "/dev/disk/by-uuid/cc86a8b4-9910-43e1-b3f9-2fe83f377807";}
-  # ];
-
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
