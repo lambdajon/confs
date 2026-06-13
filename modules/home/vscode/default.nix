825662685741df8caee07dc7f1a107b2b6def958
lambdajon <lambdajon42@gmail.com>
2026-06-13 17:54:15 +0500

    coq configs

--- modules/home/vscode/default.nix
+++ modules/home/vscode/default.nix
@@ -85,6 +85,18 @@
                 version = "3.1.5";
                 sha256 = "sha256-WIhmAZLR2WOSqQF3ozJ/Vr3Rp6HdSK7L23T3h4AVaGM=";
               }
+              {
+                name = "coq-lsp";
+                publisher = "ejgallego";
+                version = "0.2.4";
+                sha256 = "s2f2i3sNZ3EdCHDgkYPPiXDp25cViAZy+DpnDxfWaSo=";
+              }
+              {
+                name = "wasm-wasi-core";
+                publisher = "ms-vscode";
+                version = "1.0.2";
+                sha256 = "hrzPNPaG8LPNMJq/0uyOS8jfER1Q0CyFlwR42KmTz8g=";
+              }
             ];
           # User defined setings (raw json)
           userSettings = {
